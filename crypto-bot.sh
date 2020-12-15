8054255ea2bc18e67720cc3b28daa2b357459234
XternA <xternia@gmail.com>
2020-12-15 15:03:47 +0000

    Fixed script not working on Darwin

--- crypto-bot.sh
+++ crypto-bot.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-
-cd $ABSDIR
-
-while true; do
+run() {
+  while true; do
     clear && node modules/bot.js
     sleep 3
-done
+  done
+}
+
+UNAME=$( command -v uname)
+case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
+  linux*)
+    ABSPATH=$(readlink -f $0)
+    ABSDIR=$(dirname $ABSPATH)
+    cd $ABSDIR; run
+    ;;
+  darwin*)
+    DIR="$(dirname "$(readlink -f "$0")")"
+    cd $DIR; run
+    ;;
+esac
--- signup.sh
+++ signup.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
+run() {
+    clear && node modules/signup.js
+}
 
-cd $ABSDIR
-clear && node modules/signup.js
+UNAME=$( command -v uname)
+case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
+  linux*)
+    ABSPATH=$(readlink -f $0)
+    ABSDIR=$(dirname $ABSPATH)
+    cd $ABSDIR; run
+    ;;
+  darwin*)
+    DIR="$(dirname "$(readlink -f "$0")")"
+    cd $DIR; run
+    ;;
+esac
