f6eb2a1c1f1400de0f7a5b9d849c3d9552baf3b8
XternA <xternia@gmail.com>
2020-12-21 16:57:39 +0000

    Turned headless back on

--- modules/system.js
+++ modules/system.js
@@ -24,7 +24,7 @@ async function browser() {
     if (notEmpty(browserpath)) {
         return await puppeteer.launch({headless: true, executablePath: browserpath, args: args});
     }
-    return await puppeteer.launch({headless: false, args: args});
+    return await puppeteer.launch({headless: true, args: args});
 }
 
 function getJustSite(link) {
